7589d474ba43c4516234b40dad73f7fa82e967c6
Anton Nikanorov <662307@gmail.com>
2015-09-06 01:19:38 +0300

    cron

---
+++ src/main/java/ru/antowka/job/Tickets.java
@@ -0,0 +1,17 @@
+package ru.antowka.job;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.antowka.service.TicketService;
+
+/**
+ * Created by Anton Nik on 06.09.15.
+ */
+public class Tickets {
+
+    @Autowired
+    private TicketService ticketService;
+
+    public void updateCounterInSettings(){
+        System.out.println("TEST CRON IN SPRING");
+    }
+}
