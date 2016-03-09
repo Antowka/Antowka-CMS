25d6ec73d713c213cd4d953038e230ab86614ac8
Anton Nikanorov <anton@antowka.ru>
2016-03-10 00:56:11 +0300

    Test for ArticleCategoryService - finish

---
+++ src/test/java/ru/antowka/service/impl/ArticleServiceImplTest.java
@@ -0,0 +1,41 @@
+package ru.antowka.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for ArticleService
+ */
+public class ArticleServiceImplTest {
+
+    @Test
+    public void testCreateArticle() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateArticle() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveArticle() throws Exception {
+
+    }
+
+    @Test
+    public void testGetArticles() throws Exception {
+
+    }
+
+    @Test
+    public void testGetArticle() throws Exception {
+
+    }
+
+    @Test
+    public void testGetArticlesByUser() throws Exception {
+
+    }
+}
